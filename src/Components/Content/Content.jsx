a1885f19da2c5c9894d145e584757fb0e2e9dc2a
Pavel Rafeev <>
2019-11-21 12:27:49 +0200

    компоненты стартовой страницы и игры

--- src/Components/Content/Content.jsx
+++ src/Components/Content/Content.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Route } from 'react-router-dom'
-
 import inClass from './Content.module.css';
+import Welcome from './Welcome/Welcome';
+import Game from './game/Game';
 
 const Content = () => {
     return (
-        <div className={ inClass.content }>Content</div>
+        <div className={ inClass.content }>
+            <Route path='/game' component={ Game } />
+            <Route path='/welcome' component={ Welcome } />
+        </div>
     )
  }
 
---
+++ src/Components/Content/Welcome/Welcome.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import inClass from './Welcome.module.css';
+
+const Welcome = () =>  {
+    return (
+        <div>Welcome</div>
+    )
+}
+
+export default Welcome;
